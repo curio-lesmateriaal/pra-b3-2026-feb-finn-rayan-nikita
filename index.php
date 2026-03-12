7d4e03e19f212698c648dfaa1f26041b45e83fbf
NikitaMSK <nikiperetz10@gmail.com>
2026-03-12 10:20:59 +0100

    Signup pagina aafmaken

--- index.php
+++ index.php
@@ -8,10 +8,22 @@
 
 <body>
     
-    <div class="container">
-
-
+    <div class="form-container">
+        <div class="input-container">
+            <input type="text" placeholder="username" > 
+            <input type="password" placeholder="password" > 
+        </div>   
+        <div class="button">
+            <button class="submit" type="submit"><a href="login.html">Log in</a></button> 
+        </div>  
+        <div class="signup">
+            Don't Have An Account?<a href="signup.php"> sign up</a>  
+        </div>
     </div>
+    <footer>
+        <p>&copy; 2026 pra-b3 Nikita-Finn-Rayan</p>
+    </footer>
+    
 
 </body>
 
